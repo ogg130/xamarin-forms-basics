0dfe32f254c6304dc922241893db07e4951aa664
ogg130 <ogg130@gmail.com>
2019-12-28 19:00:24 -0700

    Module 5 finished

---
+++ BethanysPieShopMobile/BethanysPieShopMobile/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace BethanysPieShopMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new LoginView();
+            //MainPage = new RegisterView();
+            //MainPage = new ContactView();
+            //MainPage = new SettingsView();
+            //MainPage = new WebPageView();
+            MainPage = new PieDetailView(); 
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ BethanysPieShopMobile/BethanysPieShopMobile/Controls/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BethanysPieShopMobile
+{
+    public partial class MainPage1 : ContentPage
+    {
+        public MainPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void LabelButton_OnClicked(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new LabelPage());
+        }
+
+        private void ImageViewPageButton_OnClicked(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new ImageViewPage());
+        }
+
+        private void ButtonPageButton_OnClicked(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new ButtonPage());
+        }
+
+        private void EntryPageButton_OnClicked(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new EntryPage());
+        }
+
+        private void SliderPageButton_OnClicked(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new SliderPage());
+        }
+
+        private void TimeAndDatePageButton_OnClicked(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new TimeAndDatePickerPage());
+        }
+
+        private void OtherViewPageButton_OnClicked(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new OtherViewPage());
+        }
+    }
+}
